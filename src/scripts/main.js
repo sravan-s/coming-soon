7620f1229b2ca6dc1788da510face700479c4e6b
ess_ess <sravans2011@gmail.com>
2016-11-15 08:10:19 +0530

    In progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 (() => {
-    console.log("App init");
+  $.magicCanvas.draw({
+    type: 'random-move',
+    rgb: circlePos => {
+      let px = circlePos.x;
+      let py = circlePos.y;
+      // do some computation....
+      return {
+        r: parseInt(px % 255),
+        g: parseInt(py % 255),
+        b: 203
+      };
+    }
+  });
 })();
